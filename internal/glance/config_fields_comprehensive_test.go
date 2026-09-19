4d085c2a53110fa2b33d864c0ad1c4a7439fa3a9
Mark Hickey <2319445+samcro1967@users.noreply.github.com>
2026-09-19 15:28:54 -0500

    Add Homelab SVG Assets icon support

--- internal/glance/config_fields_comprehensive_test.go
+++ internal/glance/config_fields_comprehensive_test.go
@@ -91,7 +91,7 @@ func TestComprehensiveCustomIconField(t *testing.T) {
 	}{
 		{"https://example.invalid/icon.svg", "example.invalid", false}, {"auto-invert /icon.png", "/icon.png", true},
 		{"si:github", "simple-icons", true}, {"di:plex.png", "dashboard-icons/png/plex.png", false},
-		{"di:plex.bad", "dashboard-icons/svg/plex.svg", false}, {"mdi:home", "@mdi/svg", true}, {"sh:glance.png", "selfhst/icons@main/png/glance.png", false},
+		{"di:plex.bad", "dashboard-icons/svg/plex.svg", false}, {"hla:linux", "@loganmarchione/homelab-svg-assets@latest/assets/linux.svg", false}, {"hla:linux.png", "@loganmarchione/homelab-svg-assets@latest/assets/linux.svg", false}, {"mdi:home", "@mdi/svg", true}, {"sh:glance.png", "selfhst/icons@main/png/glance.png", false},
 		{"unknown:value", "unknown:value", false},
 	}
 	for _, tt := range tests {
